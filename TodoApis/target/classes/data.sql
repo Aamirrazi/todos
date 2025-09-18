73b2fabfaf2c78c9ce90471d2fad7f03b6156243
Aamir Razi <124306794+Aamirrazi@users.noreply.github.com>
2025-09-19 00:21:14 +0530

    complete wer

---
+++ TodoApis/target/classes/data.sql
@@ -0,0 +1,6 @@
+insert into todo (id,description,done,target_date,username)
+values(10001,'learning spring',false,CURRENT_DATE(),'in28minutes');
+insert into todo (id,description,done,target_date,username)
+values(10002,'learning dev',false,CURRENT_DATE(),'in28minutes');
+insert into todo (id,description,done,target_date,username)
+values(10003,'learning java',false,CURRENT_DATE(),'in28minutes');
